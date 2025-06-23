9fdfe8c24fcf1eb6ce34fc7ea5e9b9c864505dcd
Gustaf26 <gustavo.catalasverdrup@gmail.com>
2025-06-23 06:52:20 +0200

    (Logic > eventCreation): Starting the logic for event creation from the modal form

---
+++ src/createEvent.js
@@ -0,0 +1,7 @@
+
+
+export function createEvent(title, place, date) {
+
+    console.log(title, place, date)
+
+}
--- src/main.js
+++ src/main.js
@@ -1,18 +1,54 @@
 import { loadDb } from './db/db'
 import { showMonthCalendar } from './calendarLogik'
+import { createEvent } from './createEvent'
 
 const months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
 const thisMonth = new Date().getMonth()
 
+const closeModal = () => {
+
+  document.getElementById('modal').remove()
+
+}
+
+const showModal = () => {
+  document.getElementById('app').innerHTML += `<div id="modal">
+  <span id="close-event-on-creation" onclick="closeModal()">X</span>
+  <h3>Create Event</h3>
+  <form id="create-event-form">
+  <label for="title">Title</label>
+  <input name="title" type="text" placeholder="Enter a title" required/>
+   <label for="title">Place</label>
+  <input name="place" type="text" placeholder="Enter a place" required/>
+  <label for="date">Date</label>
+  <input name="date" type="date" required/>
+  <button>Submit</button>
+  </form>
+  </div>`
+
+  setTimeout(() => {
+    document.getElementById('create-event-form').addEventListener('submit', (e) => {
+      e.preventDefault()
+      let eventTitle = e.target[0].value
+      let eventPlace = e.target[1].value
+      let eventDate = e.target[2].value
+
+      createEvent(eventTitle, eventPlace, eventDate)
+    })
+  }, 2000)
+}
+
 document.getElementById('app').innerHTML = `<aside>
                                   <h2>This Month´s Events</h2>
                                   <ul id="events-list"></ul>
+                                  <button onclick="showModal()">+</button>
                                   </aside>
                                   <section>
                                     <h2>${months[new Date().getMonth()]}</h2>
                                     <div id="month-calendar-container">
                                     </div>
-                                  <section>`
+                                  </section>`
+
 
 const db = await loadDb()
 
@@ -43,3 +79,6 @@ const getEvents = () => {
 
 
 showMonthCalendar(getEvents())
+
+window.showModal = showModal
+window.closeModal = closeModal
